24383665c7af84bc853ae821887c113d9af8a852
Loki <subtlepseudonym@gmail.com>
2018-07-23 00:27:16 -0400

    Update public IP code, tests, and godoc comments
    Implemented GetPublicIP function so that devs can specify their own
    http client (*http.Client). Now using GetPublicIP as a part of the
    LogPublicIpAddress convenience function. Also added a tests for
    GetPublicIP.
    
    Implemented SimpleJSONResponse as a cleaned up rewrite of
    SimpleHttpResponse and deprecated SimpleHttpResponse. Also added a test
    for SimpleJSONResponse and reformatted the test for SimpleHttpResponse.
    
    Added and supplemented documentation for all functions and tests,
    including some sample code for SimpleJSONResponse

--- http/http.go
+++ http/http.go
@@ -1,88 +1,61 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
-// Unfortunately, maps are not compile-time constant
-// So, this is a bit of overkill, but it's convenient
-var statusMap map[int]string = map[int]string{
-	http.StatusContinue:                      "100 Continue",
-	http.StatusSwitchingProtocols:            "101 Switching Protocols",
-	http.StatusProcessing:                    "102 Processing",
-	http.StatusOK:                            "200 OK",
-	http.StatusCreated:                       "201 Created",
-	http.StatusAccepted:                      "202 Accepted",
-	http.StatusNonAuthoritativeInfo:          "203 Non-Authoritative Info",
-	http.StatusNoContent:                     "204 No Content",
-	http.StatusResetContent:                  "205 Reset Content",
-	http.StatusPartialContent:                "206 Partial Content",
-	http.StatusMultiStatus:                   "207 Multiple Statuses",
-	http.StatusAlreadyReported:               "208 Already Reported",
-	http.StatusIMUsed:                        "226 IM Used",
-	http.StatusMultipleChoices:               "300 Multiple Choices",
-	http.StatusMovedPermanently:              "301 Moved Permanently",
-	http.StatusFound:                         "302 Found",
-	http.StatusSeeOther:                      "303 Found",
-	http.StatusNotModified:                   "304 Not Modified",
-	http.StatusUseProxy:                      "305 Use Proxy",
-	http.StatusTemporaryRedirect:             "307 Temporary Redirect",
-	http.StatusPermanentRedirect:             "308 Permanent Redirect",
-	http.StatusBadRequest:                    "400 Bad Request",
-	http.StatusUnauthorized:                  "401 Unauthorized",
-	http.StatusPaymentRequired:               "402 Payment Required",
-	http.StatusForbidden:                     "403 Forbidden",
-	http.StatusNotFound:                      "404 Not Found",
-	http.StatusMethodNotAllowed:              "405 Method Not Allowed",
-	http.StatusNotAcceptable:                 "406 Not Acceptable",
-	http.StatusProxyAuthRequired:             "407 Proxy Authorization Required",
-	http.StatusRequestTimeout:                "408 Request Timeout",
-	http.StatusConflict:                      "409 Conflict",
-	http.StatusGone:                          "410 Gone",
-	http.StatusLengthRequired:                "411 Length Required",
-	http.StatusPreconditionFailed:            "412 Precondition Failed",
-	http.StatusRequestEntityTooLarge:         "413 Request Entity Too Large",
-	http.StatusRequestURITooLong:             "414 Request URI Too Long",
-	http.StatusUnsupportedMediaType:          "415 Unsupported Media Type",
-	http.StatusRequestedRangeNotSatisfiable:  "416 Request Range Not Satisfiable",
-	http.StatusExpectationFailed:             "417 Expectation Failed",
-	http.StatusTeapot:                        "418 Short and Stout",
-	http.StatusUnprocessableEntity:           "422 Unprocessaable Entity",
-	http.StatusLocked:                        "423 Locked",
-	http.StatusFailedDependency:              "424 Failed Dependency",
-	http.StatusUpgradeRequired:               "426 Upgrade Required",
-	http.StatusPreconditionRequired:          "428 Precondition Required",
-	http.StatusTooManyRequests:               "429 Too Many Requests",
-	http.StatusRequestHeaderFieldsTooLarge:   "431 Request Header Fields Too Large",
-	http.StatusUnavailableForLegalReasons:    "451 Unavailable For Legal Reasons",
-	http.StatusInternalServerError:           "500 Internal Server Error",
-	http.StatusNotImplemented:                "501 Not Implemented",
-	http.StatusBadGateway:                    "502 Bad Gateway",
-	http.StatusServiceUnavailable:            "503 Service Unavailable",
-	http.StatusGatewayTimeout:                "504 Gateway Timeout",
-	http.StatusHTTPVersionNotSupported:       "505 HTTP Version Not Supported",
-	http.StatusVariantAlsoNegotiates:         "506 Variant Also Negotiates",
-	http.StatusInsufficientStorage:           "507 Insufficient Storage",
-	http.StatusLoopDetected:                  "508 Loop Detected",
-	http.StatusNotExtended:                   "510 Not Extended",
-	http.StatusNetworkAuthenticationRequired: "511 Network Authentication Required",
+// This is the address used to retrieve your public IP
+const ipAPIAddress = "https://api.ipify.org?format=json"
+
+type ipResponse struct {
+	IP string `json:"ip"`
 }
 
-func LogPublicIpAddress(l *log.Logger, port int) error {
-	// NOTE: This function is intended to print the public ip of a server, so an internet connection is assumed
-	res, err := http.Get("http://ipinfo.io/ip")
+// GetPublicIP retrieves the public IP address of the computer where this function is run
+// This function will return an error if there is no internet connection and, as of v2.0.0,
+// depends upon ipify.org
+func GetPublicIP(client *http.Client) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, ipAPIAddress, nil)
 	if err != nil {
-		return err
+		return "", errors.Wrap(err, "build request failed")
 	}
 
-	ipBytes, err := ioutil.ReadAll(res.Body)
+	c := http.DefaultClient
+	if client != nil {
+		c = client
+	}
+
+	res, err := c.Do(req)
 	if err != nil {
-		return err
+		return "", errors.Wrap(err, "do request failed")
+	}
+
+	if res == nil {
+		return "", errors.New("response is nil")
+	}
+	decoder := json.NewDecoder(res.Body)
+	var ipJSON ipResponse
+	err = decoder.Decode(&ipJSON)
+	if err != nil {
+		return "", errors.Wrap(err, "decode ipResponse failed")
+	}
+
+	return ipJSON.IP, nil
+}
+
+// LogPublicIpAddress is a convenience function for getting your public IP and passing
+// it directly to a logger with predefined formatting
+func LogPublicIpAddress(l *log.Logger, port int) error {
+	ip, err := GetPublicIP(http.DefaultClient)
+	if err != nil {
+		return errors.Wrap(err, "get public IP failed")
 	}
 
 	var portString string
@@ -90,16 +63,37 @@ func LogPublicIpAddress(l *log.Logger, port int) error {
 		portString = fmt.Sprintf(":%d", port)
 	}
 	if l != nil {
-		l.Printf("Listening on %v%s\n", strings.TrimRight(string(ipBytes), "\n"), portString)
+		l.Printf("Listening on %v%s\n", strings.TrimRight(ip, "\n"), portString)
 	} else {
 		// This will use the standard logger, which prints to os.Stderr
-		log.Printf("Listening on %v%s\n", strings.TrimRight(string(ipBytes), "\n"), portString)
+		log.Printf("Listening on %v%s\n", strings.TrimRight(ip, "\n"), portString)
 	}
 	return nil
 }
 
+// SimpleJSONResponse writes the provided HTTP status code back to the client, formats
+// the message into a simple JSON object, and writes that back to the client
+//
+// 	ExampleSimpleJSONResponse() {
+// 		func SomeHandler(w http.ResponseWriter, r *http.Request) {
+// 			_, err := returnsErr()
+// 			if err != nil {
+// 				SimpleJSONResponse(w, http.StatusInternalServerError, "Something went wrong")
+// 				return
+// 			}
+// 			// Output: {"msg":"Something went wrong"}
+// 		}
+// 	}
+func SimpleJSONResponse(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{"msg":%q}`, message)
+}
+
+// SimpleHttpResponse writes the provided HTTP status code back to the client, formats
+// the message into a simple JSON object, and writes that back to the client
+// Deprecated: Use SimpleJSONResponse instead
 func SimpleHttpResponse(w http.ResponseWriter, status int, msg string) {
 	// This just returns a status header and a JSON object with a single field - "status"
 	w.WriteHeader(status)
-	io.WriteString(w, fmt.Sprintf(`{"status":"%s","msg":"%s"}`, statusMap[status], msg))
+	io.WriteString(w, fmt.Sprintf(`{"status":"%s","msg":"%s"}`, http.StatusText(status), msg))
 }
--- http/http_test.go
+++ http/http_test.go
@@ -9,31 +9,64 @@ import (
 	"testing"
 )
 
-func TestLogPublicIpAddress(t *testing.T) {
-	var buf bytes.Buffer
-	logger := log.New(&buf, "PRE: ", 0)
-	LogPublicIpAddress(logger)
-
-	matched, err := regexp.Match(`PRE: Listening on [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\n`, buf.Bytes())
+// TestGetPublicIP retrieves this computer's public IP and ensures that it matches
+// an IP regular expression
+// NOTE: This test will fail if it cannot reach the address it's using to retrieve
+// your IP. That address may change periodically and is hardcoded in source
+func TestGetPublicIP(t *testing.T) {
+	ip, err := GetPublicIP(http.DefaultClient)
 	if err != nil {
-		t.Fatalf("Error matching logger output to regexp --\n%s", err.Error())
+		t.Fatalf("GetPublicIP failed: %s", err)
+	}
+
+	matched, err := regexp.Match(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}`, []byte(ip))
+	if err != nil {
+		t.Fatalf("Regex matching failed: %s", err)
 	}
 	if !matched {
-		t.Fatalf("Logger output didn't match expected pattern --\n%s", buf.String())
+		t.Errorf("IP doesn't match regex: %s", ip)
+		t.Fail()
 	}
 }
 
-func TestSimpleHttpResponse(t *testing.T) {
+// TestLogPublicIpAddress retrieves this computer's public IP and ensures that it matches
+// an IP regular expression embedded within the expected log message
+// NOTE: This test will fail if it cannot reach the address it's using to retrieve
+// your IP. That address may change periodically and is hardcoded in source
+func TestLogPublicIpAddress(t *testing.T) {
+	var buf bytes.Buffer
+	logger := log.New(&buf, "PRE: ", 0)
+	LogPublicIpAddress(logger, 8080)
+
+	matched, err := regexp.Match(`PRE: Listening on [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:8080\n`, buf.Bytes())
+	if err != nil {
+		t.Fatalf("Error matching logger output to regexp --\n%s", err)
+	}
+	if !matched {
+		t.Errorf("Logger output didn't match expected pattern --\n%s", buf.String())
+		t.Fail()
+	}
+}
+
+// TestSimpleJSONResponse sets up a mocked server that uses SimpleJSONResponse to return
+// a message. A request is made to the mocked server and the response is checked to
+// ensure that it matches the expected output of SimpleJSONResponse
+func TestSimpleJSONResponse(t *testing.T) {
 	go func() {
-		log.Fatal(http.ListenAndServe(":40000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { SimpleHttpResponse(w, http.StatusOK, "Still alive") })))
+		err := http.ListenAndServe(":50000", http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				SimpleJSONResponse(w, http.StatusOK, "Still alive")
+			}))
+		log.Fatal(err)
 	}()
 
-	res, err := http.Get("http://localhost:40000")
+	res, err := http.Get("http://localhost:50000")
 	if err != nil {
-		t.Fatalf("Error on get request to mocked server --\n%s", err.Error())
+		t.Fatalf("Error on get request to mocked server --\n%s", err)
 	}
 	if res.StatusCode != http.StatusOK {
-		t.Fatalf("Didn't receive expected status code (200 OK) from server: %s", res.Status)
+		t.Errorf("Didn't receive expected status code (200 OK) from server: %s", res.Status)
+		t.Fail()
 	}
 
 	var simpleJson struct {
@@ -43,9 +76,46 @@ func TestSimpleHttpResponse(t *testing.T) {
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&simpleJson)
 	if err != nil {
-		t.Fatalf("Error decoding json object in server response --\n%s", err.Error())
+		t.Fatalf("Error decoding json object in server response --\n%s", err)
 	}
-	if simpleJson.Status != "200 OK" || simpleJson.Msg != "Still alive" {
-		t.Fatalf(`Didn't receive expected content ( {status: "200 OK", msg: "Still alive"} ) in json response: %+v`, simpleJson)
+	if simpleJson.Msg != "Still alive" {
+		t.Errorf(`Didn't receive expected content ( {status: "200 OK", msg: "Still alive"} ) in json response: %+v`, simpleJson)
+		t.Fail()
+	}
+}
+
+// TestSimpleHttpResponse sets up a mocked server that uses SimpleHttpResponse to return
+// a message. A request is made to the mocked server and the response is checked to
+// ensure that it matches the expected output of SimpleHttpResponse
+func TestSimpleHttpResponse(t *testing.T) {
+	go func() {
+		err := http.ListenAndServe(":40000", http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				SimpleHttpResponse(w, http.StatusOK, "Still alive")
+			}))
+		log.Fatal(err)
+	}()
+
+	res, err := http.Get("http://localhost:40000")
+	if err != nil {
+		t.Fatalf("Error on get request to mocked server --\n%s", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Didn't receive expected status code (200 OK) from server: %s", res.Status)
+		t.Fail()
+	}
+
+	var simpleJson struct {
+		Status string
+		Msg    string
+	}
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&simpleJson)
+	if err != nil {
+		t.Fatalf("Error decoding json object in server response --\n%s", err)
+	}
+	if simpleJson.Msg != "Still alive" {
+		t.Errorf(`Didn't receive expected content ( {status: "200 OK", msg: "Still alive"} ) in json response: %+v`, simpleJson)
+		t.Fail()
 	}
 }
